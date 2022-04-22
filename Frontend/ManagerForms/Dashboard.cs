858d12337489929c81a5b99c90c525371cb142ad
salma tarek <62105437+salmaatarekk@users.noreply.github.com>
2022-04-22 21:45:23 +0200

    make search by name in residents listcomplete dashboard and ResidentsList

--- Frontend/ManagerForms/Dashboard.cs
+++ Frontend/ManagerForms/Dashboard.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -17,5 +19,53 @@ namespace Frontend.Extras
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        // call for number of residents 
+        public int  getNewResidentsNumber()
+        {
+            return 5;
+        }
+        public int getWeeklyIncome()
+        {
+            return 150;
+        }
+
+        public int getNumOfWorkers()
+        {
+            return 1500;
+        }
+
+        public int getMonthlyIncome()
+        {
+            return 8000;
+        }
+        public int getCurrentStayingResidents()
+        {
+            return 8000;
+        }
+
+        public int getYearlyIncome()
+        {
+            return 180000;
+        }
+
+        private void Dashboard_Load(object sender, EventArgs e)
+        {
+            label1.Text = getNewResidentsNumber().ToString();
+
+            label16.Text = getWeeklyIncome().ToString();
+
+            label7.Text = getNumOfWorkers().ToString();
+
+            label8.Text = getMonthlyIncome().ToString();
+
+            label6.Text = getCurrentStayingResidents().ToString();
+
+            label20.Text = getYearlyIncome().ToString();
+
+        }
     }
 }
